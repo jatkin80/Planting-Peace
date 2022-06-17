106b2c74e3b99a0cb1dde6bac4ff4dc2267ddb9f
Julie <jatkin80@ford.com>
2022-06-17 10:54:20 -0500

    update styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,6 +16,7 @@ const routes: Routes = [
   { path: 'editplants', component: DeletePlantsComponent },
   { path: 'mygarden', component: MyGardenComponent },
   { path: '**', component: PageNotFoundComponent },
+
 ];
 
 @NgModule({
